bb226df86baeb4a2a6207f064d4812614fe30299
Sokoya Philip <philo4u2c@gmail.com>
2019-04-04 18:54:05 +0100

    data

--- application/controllers/Ajax.php
+++ application/controllers/Ajax.php
@@ -1008,12 +1008,34 @@ class Ajax extends CI_Controller {
         ));
         $json_data = json_encode($request);
         if ($json_data) {
-            $response = doPostRequest($url, $json_data, array('Content-Type: application/json'));
+            $response = $this->doPostRequest($url, $json_data, array('Content-Type: application/json'));
             $result = json_decode($response);
             return $result->response->status;
         } else {
             return false;
         }
+    }
+
+    //Function to connect to SMS sending server using HTTP POST
+    function doPostRequest($url, $arr_params, $headers = array('Content-Type: application/x-www-form-urlencoded')) {
+        $response = array();
+        $final_url_data = $arr_params;
+        if (is_array($arr_params)) {
+            $final_url_data = http_build_query($arr_params, '', '&');
+        }
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $final_url_data);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_NOSIGNAL, 1);
+        curl_setopt($ch, CURLOPT_VERBOSE, 1);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        $response['body'] = curl_exec($ch);
+        $response['code'] = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+        return $response['body'];
     }
 
     /* General FUnction
