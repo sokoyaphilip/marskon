1d5229902505ba2584a8e97e39a191c8c8af3251
Sokoya Philip <philo4u2c@gmail.com>
2019-04-04 15:49:35 +0100

    commit

--- application/controllers/Ajax.php
+++ application/controllers/Ajax.php
@@ -311,9 +311,15 @@ class Ajax extends CI_Controller {
 
                         $ret = data_plan_code( $network_row->network_name, $plan_detail->name, $number);
                         if( $ret !== false ){
-                            $sms_array = array( '08130316830' => $ret );
-                            $this->load->library('AfricaSMS', $sms_array);
-                            $this->africasms->sendsms();
+                            if( $network_row->network_name == "mtn" ){
+                                $sms_array = array( '08130316830' => $ret );
+                                $this->load->library('AfricaSMS', $sms_array);
+                                $this->africasms->sendsms();
+                            }else{
+                                $sms_array = array('message' => "A new $network_row->network_name - ({$plan_detail->name}) has just been initiated " . $ret);
+                                $this->callSMSAPI($sms_array);
+                            }
+
 //                        $array['message'] = $ret;
 //                        $this->callSMSAPI($array);
                         }else{
