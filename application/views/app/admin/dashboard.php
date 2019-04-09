89659e5ef7ff7fd1203e58f25740eec02f1ddba7
Sokoya Philip <philo4u2c@gmail.com>
2019-04-09 06:27:41 +0100

    commit

--- application/views/app/admin/dashboard.php
+++ application/views/app/admin/dashboard.php
@@ -253,25 +253,6 @@
                     <thead>
                     <tr>
 
-                        <th style="display: none;">
-                            <div class="media">
-                                <div class="d-flex align-self-center">id</div>
-
-                                <div class="d-flex align-self-center ml-auto">
-                            <span class="d-inline-block g-width-10 g-line-height-1 g-font-size-10">
-                            <a class="g-color-gray-light-v6 g-color-secondary--hover g-text-underline--none--hover"
-                               href="javascript:;">
-                              <i class="fa fa-angle-up"></i>
-                            </a>
-                            <a class="g-color-gray-light-v6 g-color-secondary--hover g-text-underline--none--hover"
-                               href="javascript:;">
-                              <i class="fa fa-angle-down"></i>
-                            </a>
-                          </span>
-                                </div>
-                            </div>
-                        </th>
-
                         <th>
                             <div class="media">
                                 <div class="d-flex align-self-center">Date & Time</div>
