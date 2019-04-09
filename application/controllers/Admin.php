5af27670c035daec2bdda3e947f1873e57cefd5b
Sokoya Philip <philo4u2c@gmail.com>
2019-04-09 06:57:28 +0100

    commit

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -15,7 +15,7 @@ class Admin extends CI_Controller {
         $page_data['page'] = 'home';
         $page_data['products'] = $this->site->get_result('products');
 
-        $query = "SELECT t.*, u.phone, u.name FROM transactions t LEFT JOIN users u ON(u.id = t.user_id) ORDER BY t.id DESC";
+        $query = "SELECT t.*, u.phone, u.name FROM transactions t LEFT JOIN users u ON(u.id = t.user_id) ORDER BY id DESC";
         $start = $end = $transaction ='';
         if( $this->input->post() ){
             // start empty
