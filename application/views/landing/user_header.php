e720aed99e0f5e0e530a01479d0e028eba8e9bf1
Sokoya Philip <philo4u2c@gmail.com>
2019-04-20 17:21:33 +0100

    Manage Commit

--- application/views/landing/user_header.php
+++ application/views/landing/user_header.php
@@ -156,15 +156,15 @@
                         </a>
                     </li>
 
-<!--                    <li class="u-sidebar-navigation-v1-menu-item u-side-nav--top-level-menu-item --><?php //if($page == 'subscription') echo 'has-active';?><!--">-->
-<!--                        <a class="media u-side-nav--top-level-menu-link u-side-nav--hide-on-hidden g-px-15 g-py-12"-->
-<!--                           href="--><?//= base_url('dashboard/subscription/')?><!--">-->
-<!--                          <span class="d-flex align-self-center g-pos-rel g-font-size-18 g-mr-18">-->
-<!--                          <i class="fa fa-desktop"></i>-->
-<!--                          </span>-->
-<!--                            <span class="media-body align-self-center">TV Subscription</span>-->
-<!--                        </a>-->
-<!--                    </li>-->
+                    <li class="u-sidebar-navigation-v1-menu-item u-side-nav--top-level-menu-item <?php if($page == 'subscription') echo 'has-active';?>">
+                        <a class="media u-side-nav--top-level-menu-link u-side-nav--hide-on-hidden g-px-15 g-py-12"
+                           href="<?= base_url('dashboard/subscription/')?>">
+                          <span class="d-flex align-self-center g-pos-rel g-font-size-18 g-mr-18">
+                          <i class="fa fa-desktop"></i>
+                          </span>
+                            <span class="media-body align-self-center">TV Subscription</span>
+                        </a>
+                    </li>
 <!---->
 <!--                    <li class="u-sidebar-navigation-v1-menu-item u-side-nav--top-level-menu-item --><?php //if($page == 'electricity') echo 'has-active';?><!--">-->
 <!--                        <a class="media u-side-nav--top-level-menu-link u-side-nav--hide-on-hidden g-px-15 g-py-12"-->
